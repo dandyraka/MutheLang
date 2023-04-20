db24e3fecc7a5cc26d1d06f7bf326ea96f9affc7
dandyraka <dandyraka13@gmail.com>
2023-04-21 02:04:55 +0700

    Update

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
-# MutheLang
+# Umandanify
 
-Example:  
-`Aiden depre` = `ade`  
-`gaiden` = `ga`  
-`maiden upru` = `mau`  
-`maiden kaiden naiden nes` = `makanan`  
-`saiden tepre` = `sate`  
-`copro baiden ipri nes` = `cobain`  
-`supro aiden piprines` = `suapin`  
-`maiden maiden` = `mama`  
-`taiden ngaiden nes` = `tangan`  
-`peregeteles` = `pegel`  
-`maiden tupru les` = `mantul`
+## Cara penggunaan
+- NodeJS  
+Install `npm i`  
+Run `npm start`
+
+- Python  
+`python umandanify.py < input.txt` atau  
+`echo "teks yang ingin dikonversi" | python umandanify.py`
+
+------
+
+### Rumus:  
+- suku kata a + iden  
+- suku kata i + pri  
+- suku kata u + pru  
+- suku kata e + pre  
+- suku kata o + pro  
+- huruf mati + es  
+- ng mati = strengen (ex sayang => saiden yaiden strengen)  
+- ng hidup = ngaiden (ex tangan => taiden ngaiden)
 
 [Referensi](https://www.tiktok.com/search?q=bahasa%20muthe&t=1681973816022)
--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 import inquirer from 'inquirer';
 
-const toMutheLang = (sentence) => {
-    const translatedWords = sentence
-        .split(' ')
-        .map((word) => {
-            let translatedWord = '';
-            for (const letter of word) {
-                const vowelMapping = {
-                    'a': 'aiden ',
-                    'e': 'epre ',
-                    'i': 'ipri ',
-                    'o': 'opro ',
-                    'u': 'upru '
-                };
-                translatedWord += vowelMapping[letter.toLowerCase()] || letter;
-            }
-            translatedWord = translatedWord.replace(/\b([b-df-hj-np-tv-z])(?!\w)/gi, '$1es');
-            return translatedWord;
-        });
+const umandanify = (sentence) => {
+    let vocal = false;
+    let result = "";
+    for (let i = 0; i < sentence.length; i++) {
+        const letter = sentence[i];
+        if (letter === ' ') {
+            result += letter + "| ";
+            vocal = false;
+            continue;
+        } else if ("aiueo".includes(letter)) {
+            vocal = true;
+            result += letter;
+        } else if ("bcdfghjklmnpqrstvwxyz".includes(letter)) {
+            result += letter;
+        } else {
+            result += " " + letter + " ";
+            vocal = false;
+            continue;
+        }
+        if (vocal) {
+            result += " ";
+            vocal = false;
+            continue;
+        }
+    }
+    const ss = result.replace(/\s+/g, " ");
+    result = "";
+    for (const s of ss.split(" ")) {
+        if (s.length === 3 && s.slice(0, 2) !== "ng") {
+            result += s[0] + " " + s.slice(1);
+        } else if (s.length === 4 && s.slice(0, 2) === "ng") {
+            result += s.slice(0, 2) + " " + s.slice(2);
+        } else if (s.length === 4 && s.slice(1, 4) === "nya") {
+            result += s[0] + " " + s.slice(1);
+        } else if (s.length === 4 && s.slice(1, 4) !== "nya") {
+            result += s[0] + " " + s[1] + " " + s.slice(2);
+        } else {
+            result += s;
+        }
+        result += " ";
+    }
+    result = result.replace(/i/g, "ipri")
+        .replace(/e/g, "epre")
+        .replace(/o/g, "opro")
+        .replace(/u/g, "upru")
+        .replace(/a/g, "aiden")
+        .replace(/ (\w) /g, " $1es ")
+        .replace(/^(\w) /g, "$1es ")
+        .replace(/ ng /g, " strengen ")
+        .replace(/\|/g, "")
+        .replace(/\s+/g, " ")
+        .replace(/^ | $/g, "")
+        .replace(/ ([^\w])/g, "$1");
 
-    return translatedWords.join(' ').trim().replace(/\s{2,}/g, ' ');
+    return result;
 };
 
-const unMuthe = (sentence) => {
+const deumandanify = (sentence) => {
     return sentence
         .replace(/(iden|pre|pru|pro|pri)/gi, '')
+        .replace(/(strengen)/gi, 'ng')
         .replace(/(.)(es)/gi, '$1');
 };
 
@@ -34,7 +70,7 @@ const ngapain = await inquirer
             type: 'list',
             name: 'what',
             message: 'Mau ngapain?',
-            choices: ['MutheIn', 'unMuthe']
+            choices: ['Umandanify', 'Deumandanify']
         }
     ])
     .then(({ what }) => what);
@@ -44,10 +80,10 @@ const kata = await inquirer
         {
             type: 'input',
             name: 'kata',
-            message: 'Apa?'
+            message: 'Kata yang ingin diubah:'
         }
     ])
-    .then(({ kata }) => kata);
+    .then(({ kata }) => kata.toLowerCase().trim());
 
-const hasil = (ngapain == "MutheIn") ? toMutheLang(kata) : unMuthe(kata)
-console.log(`Hasil: ${hasil}`);
+const hasil = (ngapain == "Umandanify") ? umandanify(kata) : deumandanify(kata)
+console.log('Hasil:', hasil);
